edb84c99551dbed88adaf4782c679c87d24506ed
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-05-17 19:52:29 -0400

    create routes for book module, and setup route function and main.go

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/th3khan/Go-REST-API-using-Gorm-and-Fiber/book"
+)
 
 func helloWorld(c *fiber.Ctx) error {
 	c.SendString("Hello, World!")
 	return nil
 }
 
+func setupRoutes(app *fiber.App) {
+	// books
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Put("/api/v1/book/:id", book.UpdateBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
+
 func main() {
 	app := fiber.New()
 
-	app.Get("/", helloWorld)
+	setupRoutes(app)
 
 	app.Listen(":3000")
 }
