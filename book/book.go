0a17785c7d0c4526fcd92f4a1cecbac0e958f49a
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-05-17 22:53:27 -0400

    Create book model

--- book/book.go
+++ book/book.go
@@ -1,6 +1,16 @@
 package book
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	gorm.Model
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Rating int    `json:"rating"`
+}
 
 func GetBooks(c *fiber.Ctx) error {
 	return c.SendString("Get All Books")
