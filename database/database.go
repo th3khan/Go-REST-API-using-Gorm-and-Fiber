ede1770193f07b16e5a219cdd270d70e6350bfe7
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-05-17 21:05:31 -0400

    add connection to database

---
+++ database/database.go
@@ -0,0 +1,8 @@
+package database
+
+import "gorm.io/gorm"
+
+var (
+	// DB is the database connection
+	DbConnection *gorm.DB
+)
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/th3khan/Go-REST-API-using-Gorm-and-Fiber/book"
+	"github.com/th3khan/Go-REST-API-using-Gorm-and-Fiber/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func helloWorld(c *fiber.Ctx) error {
@@ -19,9 +24,19 @@ func setupRoutes(app *fiber.App) {
 	app.Delete("/api/v1/book/:id", book.DeleteBook)
 }
 
+func initDatabase() {
+	// connect to database
+	var err error
+	database.DbConnection, err = gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connected to database successfully!")
+}
+
 func main() {
 	app := fiber.New()
-
+	initDatabase()
 	setupRoutes(app)
 
 	app.Listen(":3000")
